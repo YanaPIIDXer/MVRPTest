ce760f6f637b9d88f52e90935bfcd83a342e6b66
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-24 04:42:29 +0900

    適当なModelを定義

---
+++ Assets/Scripts/Model/TestModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+    /// <summary>
+    /// Model
+    /// </summary>
+    public class TestModel : MonoBehaviour
+    {
+        /// <summary>
+        /// 値をログ表示
+        /// </summary>
+        /// <param name="Value">ログ表示する値</param>
+        private void ShowValue(float Value)
+        {
+            Debug.Log(string.Format("Value:{0}", Value));
+        }
+    }
+}
