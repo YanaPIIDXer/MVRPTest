edbb365acb4bdc17e66f1da795fcc91aed6ed4b5
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-24 05:23:41 +0900

    Presenterを定義

---
+++ Assets/Scripts/Presenter/TestPresenter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using View;
+
+namespace Presenter
+{
+    /// <summary>
+    /// Presenter
+    /// </summary>
+    public class TestPresenter : MonoBehaviour
+    {
+        /// <summary>
+        /// View
+        /// </summary>
+        private TestView View = null;
+
+        void Awake()
+        {
+            View = GetComponent<TestView>();
+        }
+    }
+}
