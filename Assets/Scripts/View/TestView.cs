4fd093774ed20021e5f3100ffb845870122ca98f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-24 04:37:57 +0900

    適当にViewを作成

---
+++ Assets/Scripts/View/TestView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace View
+{
+    /// <summary>
+    /// View
+    /// </summary>
+    public class TestView : MonoBehaviour
+    {
+        /// <summary>
+        /// スライダー
+        /// </summary>
+        [SerializeField]
+        private Slider ValueSlider = null;
+
+        /// <summary>
+        /// スライダーの値
+        /// </summary>
+        [SerializeField]
+        private Text SliderValueText = null;
+
+        /// <summary>
+        /// スライダーの値を外に放り投げるボタン
+        /// </summary>
+        [SerializeField]
+        private Button SliderValueSendButton = null;
+    }
+}
